ac751b26f3b505a1be58a388bdb9c97978ba0dc7
wjs0414 <uknow0414@naver.com>
2021-12-29 21:38:30 +0900

    Initial Commit

---
+++ MyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDiary
+//
+//  Created by Jisoo Woo on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
